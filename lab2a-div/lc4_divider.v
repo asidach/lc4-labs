ecb58b56866450443996b5980690260d1d6d95ae
asidach <asidach@seas.upenn.edu>
2022-02-02 23:39:44 -0500

    update

--- lab2a-div/lc4_divider.v
+++ lab2a-div/lc4_divider.v
@@ -10,6 +10,11 @@ module lc4_divider(input  wire [15:0] i_dividend,
 
       /*** YOUR CODE HERE ***/
 
+      genvar i;
+      for (i = 0; i < 16; i = i+1) begin
+            lc4_divider_one_iter
+      end
+
 endmodule // lc4_divider
 
 module lc4_divider_one_iter(input  wire [15:0] i_dividend,
@@ -21,5 +26,18 @@ module lc4_divider_one_iter(input  wire [15:0] i_dividend,
                             output wire [15:0] o_quotient);
 
       /*** YOUR CODE HERE ***/
+      o_remainder = i_remainder << 1;
+      o_dividend = (i_dividend >> 15) & 1b1;
+      wire i_rem_div = o_dividend | o_remainder;
+      begin
+            if (i_remainder < i_divisor)
+                  o_quotient = i_quotient << 1;
+            else 
+                  o_quotient = (i_quotient << 1) | 1b1;
+                  wire remainder_copy = o_remainder;
+                  o_remainder = remainder_copy - i_divisor - skhfglaoegfsd
+      end
+      wire dividend_copy = o_dividend;
+      o_dividend = dividend_copy << 1;
 
 endmodule
